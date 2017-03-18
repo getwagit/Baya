918eee548ae9ff1a32caaa41f453d3e29dcf516e
Markus Riegel <riegel.markus@googlemail.com>
2017-03-18 20:46:21 +0100

    (add) frame layout tests

---
+++ BayaTests/BayaFrameTests.swift
@@ -0,0 +1,78 @@
+//
+// Copyright (c) 2017 wag it GmbH.
+// License: MIT
+//
+
+import Foundation
+
+import XCTest
+@testable import Baya
+
+class BayaFrameTests: XCTestCase {
+    var l1: TestLayoutable!
+    var l2: TestLayoutable!
+    var l3: TestLayoutable!
+
+    override func setUp() {
+        super.setUp()
+        l1 = TestLayoutable()
+        l2 = TestLayoutable()
+        l3 = TestLayoutable()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        l1 = nil
+        l2 = nil
+        l3 = nil
+    }
+
+    func testEmptyArray() {
+        var layout = [TestLayoutable]().layoutFrame()
+        layout.layoutWith(frame: CGRect())
+        XCTAssert(true) // Does not crash.
+    }
+
+    func testSizes() {
+        var layout = [l1, l2, l3]
+            .layoutFrame()
+
+        l1.layoutMargins = UIEdgeInsets(
+            top: 8,
+            left: 7,
+            bottom: 4,
+            right: 3)
+        l2.layoutMargins = UIEdgeInsets(
+            top: 20,
+            left: 50,
+            bottom: 23,
+            right: 12)
+        l3.layoutMargins = UIEdgeInsets.zero
+
+        layout.layoutWith(
+            frame: CGRect(
+                x: 5,
+                y: 10,
+                width: 300,
+                height: 300))
+
+        XCTAssertEqual(l1.frame, CGRect(
+            x: 5 + l1.layoutMargins.left,
+            y: 10 + l1.layoutMargins.top,
+            width: 300 - l1.horizontalMargins,
+            height: 300 - l1.verticalMargins),
+            "unexpected l1 frame")
+        XCTAssertEqual(l2.frame, CGRect(
+            x: 5 + l2.layoutMargins.left,
+            y: 10 + l2.layoutMargins.top,
+            width: 300 - l2.horizontalMargins,
+            height: 300 - l2.verticalMargins),
+            "unexpected l2 frame")
+        XCTAssertEqual(l3.frame, CGRect(
+            x: 5 + l3.layoutMargins.left,
+            y: 10 + l3.layoutMargins.top,
+            width: 300 - l3.horizontalMargins,
+            height: 300 - l3.verticalMargins),
+            "unexpected l3 frame")
+    }
+}
