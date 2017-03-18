0bc2feda169263b1c4f7cd86326e3472c4007a57
Markus Riegel <riegel.markus@googlemail.com>
2017-03-18 19:21:44 +0100

    (impr) simplify convenience methods

--- Baya/BayaLayoutable.swift
+++ Baya/BayaLayoutable.swift
@@ -17,6 +17,31 @@ public protocol BayaLayoutable {
     mutating func layoutWith(frame: CGRect)
 }
 
+/**
+    Internal helper.
+*/
+internal extension BayaLayoutable {
+    var verticalMargins: CGFloat {
+        return layoutMargins.top + layoutMargins.bottom
+    }
+
+    var horizontalMargins: CGFloat {
+        return layoutMargins.left + layoutMargins.right
+    }
+
+    var heightWithMargins: CGFloat {
+        return frame.height + verticalMargins
+    }
+
+    var widthWithMargins: CGFloat {
+        return frame.width + horizontalMargins
+    }
+
+    func sizeThatFitsWithMargins(size: CGSize) -> CGSize {
+        return sizeThatFits(size.subtractMargins(ofElement: self))
+    }
+}
+
 // MARK: UIKit specific extensions
 
 /**
@@ -46,6 +71,32 @@ internal extension UIView {
             self.bounds = bounds
             self.center = CGPoint(x: frame.minX + bounds.midX, y: frame.minY + bounds.midY)
         }
+    }
+}
+
+// MARK: CGRect and CGSize
+
+internal extension CGSize {
+    func subtractMargins(ofElement element: BayaLayoutable) -> CGSize {
+        return CGSize(
+            width: max(width - element.horizontalMargins, 0),
+            height: max(height - element.verticalMargins, 0))
+    }
+
+    func addMargins(ofElement element: BayaLayoutable) -> CGSize {
+        return CGSize(
+            width: width + element.horizontalMargins,
+            height: height + element.verticalMargins)
+    }
+}
+
+internal extension CGRect {
+    func subtractMargins(ofElement element: BayaLayoutable) -> CGRect {
+        return CGRect(
+            origin: CGPoint(
+                x: minX + element.layoutMargins.left,
+                y: minY + element.layoutMargins.top),
+            size: size.subtractMargins(ofElement: element))
     }
 }
 
