b18f51c8cb4c2d97e882fc5a21c1e4a2a66dbc94
Markus Riegel <riegel.markus@googlemail.com>
2017-06-27 15:49:55 +0200

    (add) group layout and test

---
+++ Baya/layouts/BayaGroupLayout.swift
@@ -0,0 +1,74 @@
+//
+// Copyright (c) 2017 wag it GmbH.
+// License: MIT
+//
+
+import Foundation
+
+/**
+    Simple layout that stacks Layoutables.
+    The frame of the Layoutables is not adjusted. BayaGroupLayout measures each element
+    and applies the size it requested.
+*/
+public struct BayaGroupLayout: BayaLayout, BayaLayoutIterator {
+    public var layoutMargins: UIEdgeInsets
+    public var frame: CGRect
+
+    private var elements: [BayaLayoutable]
+    private var measures = [CGSize]()
+
+    init(
+        elements: [BayaLayoutable],
+        layoutMargins: UIEdgeInsets = UIEdgeInsets.zero) {
+        self.elements = elements
+        self.layoutMargins = layoutMargins
+        self.frame = CGRect()
+    }
+
+    mutating public func layoutWith(frame: CGRect) {
+        self.frame = frame
+        iterate(&elements, measures) {
+            e1, e2, e2s in
+            let size = saveMeasure(e2s: e2s, e2: &e2, size: frame.size)
+            return CGRect(
+                origin: CGPoint(
+                    x: frame.minX + e2.layoutMargins.left,
+                    y: frame.minY + e2.layoutMargins.top),
+                size: size)
+        }
+    }
+
+    public mutating func sizeThatFits(_ size: CGSize) -> CGSize {
+        measures = measure(&elements, size: size)
+        var maxWidth: CGFloat = 0
+        var maxHeight: CGFloat = 0
+        for i in 0..<elements.count {
+            let element = elements[i]
+            let elementSize = measures[i]
+            maxWidth = max(maxWidth, elementSize.width + element.horizontalMargins)
+            maxHeight = max(maxHeight, elementSize.height + element.verticalMargins)
+        }
+        return CGSize(width: maxWidth, height: maxHeight)
+    }
+}
+
+public extension Sequence where Iterator.Element: BayaLayoutable {
+    /**
+        Groups the layoutables together.
+        Layoutables are given the size that they request.
+    */
+    func layoutAsGroup(layoutMargins: UIEdgeInsets = UIEdgeInsets.zero) -> BayaGroupLayout {
+        return BayaGroupLayout(elements: self.array(), layoutMargins: layoutMargins)
+    }
+}
+
+public extension Sequence where Iterator.Element == BayaLayoutable {
+    /**
+        Groups the layoutables together.
+        Layoutables are given the size that they request.
+    */
+    func layoutAsGroup(layoutMargins: UIEdgeInsets = UIEdgeInsets.zero) -> BayaGroupLayout {
+        return BayaGroupLayout(elements: self.array(), layoutMargins: layoutMargins)
+    }
+}
+
---
+++ BayaTests/BayaGroupTests.swift
@@ -0,0 +1,92 @@
+//
+// Copyright (c) 2017 wag it GmbH.
+// License: MIT
+//
+
+import Foundation
+
+import XCTest
+@testable import Baya
+
+class BayaGroupTests: XCTestCase {
+    var l1: TestLayoutable!
+    var l2: TestLayoutable!
+    var l3: TestLayoutable!
+
+    override func setUp() {
+        super.setUp()
+        l1 = TestLayoutable()
+        l2 = TestLayoutable()
+        l3 = TestLayoutable()
+
+        l1.layoutMargins = UIEdgeInsets(
+            top: 8,
+            left: 7,
+            bottom: 4,
+            right: 3)
+        l2.layoutMargins = UIEdgeInsets(
+            top: 20,
+            left: 50,
+            bottom: 23,
+            right: 12)
+        l3.layoutMargins = UIEdgeInsets.zero
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        l1 = nil
+        l2 = nil
+        l3 = nil
+    }
+
+    func testEmptyArray() {
+        var layout = [TestLayoutable]().layoutAsGroup()
+        layout.startLayout(
+            with: CGRect())
+        XCTAssert(true) // Does not crash.
+    }
+
+    func testSizes() {
+        var layout = [l1, l2, l3]
+            .layoutAsGroup()
+        let layoutRect = CGRect(
+            x: 5,
+            y: 10,
+            width: 300,
+            height: 300)
+        layout.startLayout(with: layoutRect)
+
+        XCTAssertEqual(l1.frame, CGRect(
+            x: layoutRect.origin.x + l1.layoutMargins.left,
+            y: layoutRect.origin.y + l1.layoutMargins.top,
+            width: TestLayoutable.sideLength,
+            height: TestLayoutable.sideLength),
+            "unexpected l1 frame")
+        XCTAssertEqual(l2.frame, CGRect(
+            x: layoutRect.origin.x + l2.layoutMargins.left,
+            y: layoutRect.origin.y + l2.layoutMargins.top,
+            width: TestLayoutable.sideLength,
+            height: TestLayoutable.sideLength),
+            "unexpected l2 frame")
+        XCTAssertEqual(l3.frame, CGRect(
+            x: layoutRect.origin.x + l3.layoutMargins.left,
+            y: layoutRect.origin.y + l3.layoutMargins.top,
+            width: TestLayoutable.sideLength,
+            height: TestLayoutable.sideLength),
+            "unexpected l3 frame")
+    }
+
+    func testMeasures() {
+        var layout = [l1, l2, l3]
+            .layoutAsGroup()
+        let size = layout.sizeThatFits(CGSize(
+            width: 300,
+            height: 200))
+
+        // l2 has the biggest margins and should define the size.
+        XCTAssertEqual(size, CGSize(
+            width: TestLayoutable.sideLength + l2.horizontalMargins,
+            height: TestLayoutable.sideLength + l2.verticalMargins),
+            "sizes don't match")
+    }
+}
