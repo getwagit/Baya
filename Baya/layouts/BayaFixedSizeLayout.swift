45a673f84adf5e8ab61abb3a4ca8d1e7838dfde4
Markus Riegel <riegel.markus@googlemail.com>
2017-06-28 16:29:43 +0200

    (impr) fixed size layout respects modes

--- Baya/layouts/BayaFixedSizeLayout.swift
+++ Baya/layouts/BayaFixedSizeLayout.swift
@@ -7,53 +7,70 @@ import Foundation
 import UIKit
 
 /**
-    A layout that uses a fixed size to layout its element.
+    Wraps a layoutable and overrides layout modes by applying fixed sizes.
+    Mirrors layout modes from its child.
 */
 public struct BayaFixedSizeLayout: BayaLayout {
-    public var layoutMargins: UIEdgeInsets
-    public var frame: CGRect
+    public var layoutMargins = UIEdgeInsets.zero
+    public var frame = CGRect()
+    public var layoutModes: BayaLayoutModes {
+        return element.layoutModes
+    }
     private var element: BayaLayoutable
+    private var measure: CGSize?
     private var fixedWidth: CGFloat?
     private var fixedHeight: CGFloat?
 
     init(
         element: BayaLayoutable,
         width: CGFloat?,
-        height: CGFloat?,
-        layoutMargins: UIEdgeInsets = UIEdgeInsets.zero) {
+        height: CGFloat?) {
         self.fixedWidth = width
         self.fixedHeight = height
         self.element = element
-        self.layoutMargins = layoutMargins
-        self.frame = CGRect()
     }
 
     mutating public func layoutWith(frame: CGRect) {
         self.frame = frame
-        element.subtractMarginsAndLayoutWith(frame: frame)
+        let size: CGSize;
+        if let fixedHeight = fixedHeight, let fixedWidth = fixedWidth {
+            size = CGSize(
+                width: fixedWidth - element.horizontalMargins,
+                height: fixedHeight - element.verticalMargins)
+        } else {
+            let defaultSize = calculateSizeForLayout(forChild: &element, cachedSize: measure, ownSize: frame.size)
+            size = CGSize(
+                width: fixedWidth != nil ? fixedWidth! - element.horizontalMargins : defaultSize.width,
+                height: fixedHeight != nil ? fixedHeight! - element.verticalMargins : defaultSize.height)
+        }
+
+        element.layoutWith(frame: CGRect(
+            origin: CGPoint(
+                x: frame.origin.x + element.layoutMargins.left,
+                y: frame.origin.y + element.layoutMargins.top),
+            size: size))
     }
 
     public mutating func sizeThatFits(_ size: CGSize) -> CGSize {
-        let fit = element.sizeThatFitsWithMargins(size)
+        measure = element.sizeThatFitsWithMargins(size)
         return CGSize(
-            width: fixedWidth ?? (fit.width + element.horizontalMargins),
-            height: fixedHeight ?? (fit.height + element.verticalMargins))
+            width: fixedWidth ?? (measure!.width + element.horizontalMargins),
+            height: fixedHeight ?? (measure!.height + element.verticalMargins))
     }
 }
 
 public extension BayaLayoutable {
     /**
         Gives this element a fixed sized container.
+        Mirrors layout modes that are not fixed sizes from this element.
     */
     func layoutWithFixedSize(
         width: CGFloat?,
-        height: CGFloat?,
-        layoutMargins: UIEdgeInsets = UIEdgeInsets.zero)
+        height: CGFloat?)
             -> BayaFixedSizeLayout {
         return BayaFixedSizeLayout(
             element: self,
             width: width,
-            height: height,
-            layoutMargins: layoutMargins)
+            height: height)
     }
 }
