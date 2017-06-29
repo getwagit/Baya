4282ea3fb954305e4a1c86a9ca9364e70eba002e
Markus Riegel <riegel.markus@googlemail.com>
2017-06-29 16:16:32 +0200

    (impr) scroll layout respects layout modes

--- Baya/layouts/BayaScrollLayout.swift
+++ Baya/layouts/BayaScrollLayout.swift
@@ -12,6 +12,7 @@ import UIKit
 public struct BayaScrollLayout: BayaLayout {
     public var layoutMargins: UIEdgeInsets
     public var frame: CGRect
+    public let layoutModes: BayaLayoutModes
     var orientation: BayaLayoutOptions.Orientation
 
     private var container: ScrollLayoutContainer
@@ -28,29 +29,76 @@ public struct BayaScrollLayout: BayaLayout {
         self.orientation = orientation
         self.layoutMargins = layoutMargins
         self.frame = CGRect()
+        self.layoutModes = BayaLayoutModes(
+            width: content.layoutModes.width == .wrapContent && container.layoutModes.width == .wrapContent ?
+                .wrapContent : .matchParent,
+            height: content.layoutModes.height == .wrapContent && container.layoutModes.height == .wrapContent ?
+                .wrapContent : .matchParent)
     }
 
     mutating public func layoutWith(frame: CGRect) {
         self.frame = frame
-        let contentSize = contentMeasure ?? measureContent(frame.size)
+        let maximalAvailableContentSize = sizeForMeasurement(frame.size)
+        let measuredContentSize = contentMeasure ?? content.sizeThatFits(maximalAvailableContentSize)
+        let adjustedContentSize: CGSize
+        switch orientation {
+        case .horizontal:
+            adjustedContentSize = CGSize(
+                width: content.layoutModes.width == .wrapContent ?
+                    measuredContentSize.width : max(measuredContentSize.width, maximalAvailableContentSize.width),
+                height: content.layoutModes.height == .wrapContent ?
+                    measuredContentSize.height : frame.height - content.verticalMargins)
+        case .vertical:
+            adjustedContentSize = CGSize(
+                width: content.layoutModes.width == .wrapContent ?
+                    measuredContentSize.width : frame.width - content.horizontalMargins,
+                height: content.layoutModes.height == .wrapContent ?
+                    measuredContentSize.height : max(measuredContentSize.height, maximalAvailableContentSize.height)
+            )
+        }
+
+        let minContainerWidth = measuredContentSize.width + content.horizontalMargins
+        let minContainerHeight = measuredContentSize.height + content.verticalMargins
+        let maxContainerWidth = frame.width - container.horizontalMargins
+        let maxContainerHeight = frame.height - container.verticalMargins
+        let containerSize = CGSize(
+            width: container.layoutModes.width == .wrapContent && content.layoutModes.width == .wrapContent ?
+                min(minContainerWidth, maxContainerWidth) : maxContainerWidth,
+            height: container.layoutModes.height == .wrapContent && content.layoutModes.height == .wrapContent ?
+                min(minContainerHeight, maxContainerHeight) : maxContainerHeight)
 
         content.layoutWith(frame: CGRect(
-            origin: CGPoint(),
-            size: contentSize))
-        container.layoutWith(frame: frame.subtractMargins(ofElement: container))
-        container.contentSize = contentSize
+            origin: CGPoint(
+                x: content.layoutMargins.left,
+                y: content.layoutMargins.top),
+            size: adjustedContentSize))
+        container.layoutWith(frame: CGRect(
+            origin: CGPoint(
+                x: container.layoutMargins.left,
+                y: container.layoutMargins.top),
+            size: containerSize))
+        container.contentSize = adjustedContentSize.addMargins(ofElement: content)
     }
 
     mutating public func sizeThatFits(_ size: CGSize) -> CGSize {
-        contentMeasure = measureContent(size)
-        // ScrollLayout always fits the given frame
-        return size
+        contentMeasure = content.sizeThatFits(sizeForMeasurement(size))
+        return CGSize(
+            width: min(contentMeasure!.width + content.horizontalMargins, size.width),
+            height: min(contentMeasure!.height + content.verticalMargins, size.height))
     }
 
-    mutating private func measureContent(_ size: CGSize) -> CGSize {
-        return orientation == .horizontal ?
-            content.sizeThatFits(CGSize(width: CGFloat.greatestFiniteMagnitude, height: size.height)) :
-            content.sizeThatFits(CGSize(width: size.width, height: CGFloat.greatestFiniteMagnitude))
+    private func sizeForMeasurement(_ size: CGSize) -> CGSize {
+        let availableWidth: CGFloat
+        let availableHeight: CGFloat
+        switch orientation {
+        case .horizontal:
+            availableWidth = CGFloat.greatestFiniteMagnitude
+            availableHeight = size.height - content.verticalMargins - container.verticalMargins
+        case .vertical:
+            availableWidth = size.width - content.horizontalMargins - container.horizontalMargins
+            availableHeight = CGFloat.greatestFiniteMagnitude
+        }
+        return CGSize(width: availableWidth, height: availableHeight)
     }
 }
 
