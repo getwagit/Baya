8c705aed898220d66f201f6e40d65a61944d19cd
Markus Riegel <riegel.markus@googlemail.com>
2017-06-30 10:55:36 +0200

    (impr) scroll test measures

--- BayaTests/BayaScrollTests.swift
+++ BayaTests/BayaScrollTests.swift
@@ -3,25 +3,53 @@
 // License: MIT
 //
 
-//import Foundation
-//import XCTest
-//@testable import Baya
-//
-//class BayaScrollTests: XCTestCase {
-//    var content: TestLayoutable!
-//    var container:
-//    let layoutRect = CGRect(x: 3, y: 4, width: 300, height: 400)
-//
-//    override func setUp() {
-//        super.setUp()
-//        l = TestLayoutable()
-//        l.m(1, 2, 3, 4)
-//    }
-//
-//    override func tearDown() {
-//        super.tearDown()
-//        l = nil
-//    }
-//}
-//
-//class TestScrollContainer: TestLayoutable, ScrollLayoutContainer
+import Foundation
+import XCTest
+@testable import Baya
+
+class BayaScrollTests: XCTestCase {
+    var content: TestLayoutable!
+    var container: TestScrollLayoutContainer!
+    let layoutRect = CGRect(x: 3, y: 4, width: 300, height: 400)
+
+    override func setUp() {
+        super.setUp()
+        content = TestLayoutable()
+        content.m(1, 2, 3, 4)
+        container = TestScrollLayoutContainer()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        content = nil
+        container = nil
+    }
+
+    func testSmallTestContentMeasuresCorrectly() {
+        content = TestLayoutable(sideLength: 20)
+        var layout = content.layoutScrollContent(container: container)
+        let measure = layout.sizeThatFits(layoutRect.size)
+
+        XCTAssertEqual(
+            measure,
+            CGSize(
+                width: content.sideLength + content.horizontalMargins,
+                height: content.sideLength + content.verticalMargins),
+            "size does not match")
+    }
+
+    func testLargeTestContentMeasureIsLimitedByFrameSize() {
+        content = TestLayoutable(sideLength: 1345)
+        var layout = content.layoutScrollContent(container: container)
+        let measure = layout.sizeThatFits(layoutRect.size)
+
+        XCTAssertEqual(
+            measure,
+            CGSize(
+                width: layoutRect.width,
+                height: layoutRect.height),
+            "size does not match")
+    }
+
+    
+}
