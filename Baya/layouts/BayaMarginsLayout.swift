a58bb60c29fc3cc43cba7534fa9effc886bad173
Jogga <joachim@froestl.com>
2017-08-07 10:01:56 +0200

    (impr) mirror frame, remove measure

--- Baya/layouts/BayaMarginsLayout.swift
+++ Baya/layouts/BayaMarginsLayout.swift
@@ -14,27 +14,25 @@ internal struct BayaMarginsLayout: BayaLayout {
     public var layoutModes: BayaLayoutOptions.Modes {
         return element.layoutModes
     }
+    public var frame: CGRect {
+        return element.frame
+    }
     public var layoutMargins: UIEdgeInsets
-    public var frame: CGRect
     private var element: BayaLayoutable
-    private var measure: CGSize?
     
     init(
         element: BayaLayoutable,
         layoutMargins: UIEdgeInsets) {
         self.element = element
         self.layoutMargins = layoutMargins
-        self.frame = CGRect()
     }
 
     public mutating func layoutWith(frame: CGRect) {
-        self.frame = frame
         element.layoutWith(frame: frame)
     }
 
     public mutating func sizeThatFits(_ size: CGSize) -> CGSize {
-        measure = element.sizeThatFitsWithMargins(size)
-        return measure!
+        return element.sizeThatFitsWithMargins(size)
     }
 }
 
