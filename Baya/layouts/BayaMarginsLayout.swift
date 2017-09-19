d0c3d81bfbb1d161b73a1734b8e2fb00283e5984
Jogga <joachim@froestl.com>
2017-09-19 15:55:50 +0200

    (chore) comments for margins layout

--- Baya/layouts/BayaMarginsLayout.swift
+++ Baya/layouts/BayaMarginsLayout.swift
@@ -37,6 +37,10 @@ internal struct BayaMarginsLayout: BayaLayout {
 }
 
 internal extension BayaLayoutable {
+    /// Disregards the element's margins and substitutes them with the margins passed as parameter. 
+    /// Useful when the element's margins cannot be controlled otherwise, which is the case for the root view of an UIViewController.
+    /// - parameter layoutMargins: The layout's margins.
+    /// - returns: A BayaMarginsLayout.
     func layoutWithMargins(layoutMargins: UIEdgeInsets) -> BayaMarginsLayout {
         return BayaMarginsLayout(
             element: self,
@@ -45,11 +49,11 @@ internal extension BayaLayoutable {
 }
 
 extension UIViewController {
-    /**
-     The layoutMargins of UIViewController's root view are managed by the system, and cannot be changed.
-     To prevent unwanted layout behavior, use this function, which enables full control over
-     the root view's margins by wrapping the it in a BayaMarginsLayout.
-     */
+    /// Enables full control over the root view's margins by wrapping the it in a BayaMarginsLayout. 
+    /// The layoutMargins of UIViewController's root view are managed by the system, and cannot be changed.
+    /// To prevent unwanted layout behavior, use this function
+    /// - parameter layoutMargins: The desired layoutMargins around the view.
+    /// - returns: A BayaMarginsLayout containing the ViewController's root view.
     public func getRootViewAsLayoutable(with layoutMargins: UIEdgeInsets = UIEdgeInsets.zero) -> BayaLayoutable {
         return view.layoutWithMargins(layoutMargins: layoutMargins)
     }
